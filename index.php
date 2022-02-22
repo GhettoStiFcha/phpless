413add5bb67c054ca3521ad090279969a18ebd9c
ageless <slava3626@gmail.com>
2022-02-22 13:56:17 +0300

    initial commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <?php
+        $link = '/htdocs';
+    ?>
+</head>
+<body style="background-color: grey;">
+    
+    <header style="display: flex; max-height: 100px;"><a href="#"><img style="max-height: 100px;" src="https://dpo.mospolytech.ru/content/themes/default/img/logo.png" alt="logo"></a><p>Задание №1</p></header>
+    <main><a href="<?=$link?>">ссылка на <?=$link?></a></main>
+    <footer>Создать веб-страницу с динамическим контентом. Загрузить код в удаленный репозиторий. Залить на хостинг.</footer>
+
+</body>
+</html>
